9f5088870d6734cebbbe9937f40b89b04ebd206b
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2018-06-12 23:51:27 +0900

    until Compund Types at Basics

--- lf/Basics.v
+++ lf/Basics.v
@@ -21,4 +21,121 @@ Definition next_weekday (d:day) : day :=
 Compute (next_weekday friday).
 Compute (next_weekday (next_weekday saturday)).
 
-Example
+Example test_next_weekday:
+  (next_weekday (next_weekday saturday)) = tuesday.
+
+Proof.
+  simpl.
+  reflexivity.
+Qed.
+
+Inductive bool : Type :=
+| true  : bool
+| false : bool.
+
+Definition negb (b:bool) : bool :=
+  match b with
+  | true => false
+  | fase => true
+  end.
+
+Definition andb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => b2
+  | false => false
+  end.
+
+Definition orb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => true
+  | false => b2
+  end.
+
+Example test_orb1: (orb true false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_orb2: (orb false false) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_orb3: (orb false true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_orb4: (orb true true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Notation "x && y" := (andb x y).
+Notation "x || y" := (orb x y).
+
+Example test_orb5: false || false || true = true.
+Proof. simpl. reflexivity. Qed.
+
+(*
+
+  1d star. nandb.
+  The function should return true if either or both of its inputs are false.
+ *)
+
+Definition nandb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => negb b2
+  | false => true
+  end.
+
+Example test_nandb1: (nandb true false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb2: (nandb false false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb3: (nandb false true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb4: (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool :=
+  (b1 && b2 && b3).
+
+Example test_and31: (andb3 true true true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_and32: (andb3 false true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_and33: (andb3 true false true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_and34: (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed.
+
+Check true.
+(* ===> true : bool *)
+Check (negb true).
+(* ===> negb true : bool *)
+Check negb.
+(* ===> negb : bool -> bool *)
+
+Inductive rgb : Type :=
+| red : rgb
+| green : rgb
+| blue : rgb.
+
+Inductive color : Type :=
+  | black : color
+  | white : color
+  | primary : rgb -> color.
+
+Definition monochrome (c : color) : bool :=
+  match c with
+  | black => true
+  | white => false
+  | primary p => false
+  end.
+
+Definition isred (c : color) : bool :=
+  match c with
+  | black => false
+  | white => false
+  | primary red => true
+  | primary _ => false
+  end.
